e464db9ba8415d89b96e52d9ddb875e6940b24cf
len <reelaiah.l@icloud.com>
2023-10-29 16:03:34 +0800

    完成部门增删改查功能-完成代码合并的冲突

--- ui/src/api/model/basis_org.js
+++ ui/src/api/model/basis_org.js
@@ -3,11 +3,7 @@
  * @Author: reel
  * @Date: 2023-06-24 08:35:55
  * @LastEditors: reel
-<<<<<<< HEAD
- * @LastEditTime: 2023-09-29 21:57:53
-=======
- * @LastEditTime: 2023-10-28 21:16:57
->>>>>>> feat-org
+ * @LastEditTime: 2023-10-29 16:02:28
  * @Description: 请填写简介
  */
 import config from "@/config"
@@ -38,68 +34,6 @@ export default {
 
 	},
 
-<<<<<<< HEAD
-	// // 角色操作
-	// roles:{
-	// 	url: `${config.API_URL}/basis/roles`,
-	// 	name: "角色列表操作",
-
-	// 	// 单行新增
-	// 	add: async function(data={}){
-	// 		return await http.put(`${config.API_URL}/basis/auth/roles/add`, data);
-	// 	},
-		
-	// 	// 查询列表
-	// 	list: async function(data={}){
-	// 		return await http.get(`${config.API_URL}/basis/auth/roles/list`, data);
-	// 	},
-		
-	// 	// 批量更新, 
-	// 	// 但根据业务, 不允许赋予多个角色相同的权限
-	// 	// 但允许批量停用, 删除等
-	// 	edit: async function(data={}){
-	// 		return await http.post(`${config.API_URL}/basis/auth/roles/edit`, data);
-	// 	},
-
-	// 	// 批量删除
-	// 	delete: async function(data={}){
-	// 		return await http.delete(`${config.API_URL}/basis/auth/roles/delete`, data);
-	// 	},
-
-	// 	// 获取权限菜单树表
-	// 	permission: async function(data={}){
-	// 		return await http.get(`${config.API_URL}/basis/roles/auth/permission`, data);
-	// 	},
-	// },
-
-	// // 角色操作
-	// menus:{
-	// 	url: `${config.API_URL}/basis/menus`,
-	// 	name: "菜单列表操作",
-
-	// 	// 单行新增
-	// 	add: async function(data={}){
-	// 		return await http.put(`${config.API_URL}/basis/auth/menus/add`, data);
-	// 	},
-		
-	// 	// 查询列表
-	// 	list: async function(data={}){
-	// 		return await http.get(`${config.API_URL}/basis/auth/menus/list`, data);
-	// 	},
-		
-	// 	// 批量更新, 
-	// 	// 但根据业务, 不允许赋予多个角色相同的权限
-	// 	// 但允许批量停用, 删除等
-	// 	edit: async function(data={}){
-	// 		return await http.post(`${config.API_URL}/basis/auth/menus/edit`, data);
-	// 	},
-
-	// 	// 批量删除
-	// 	delete: async function(data={}){
-	// 		return await http.delete(`${config.API_URL}/basis/auth/menus/delete`, data);
-	// 	},
-	// }
-=======
 	department:{
 		url: `${config.API_URL}/basis/org/department/`,
 		name: "用户列表操作",
@@ -125,5 +59,4 @@ export default {
 		},
 
 	},
->>>>>>> feat-org
 }
